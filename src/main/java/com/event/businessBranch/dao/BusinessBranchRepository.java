b58792b4ecda22bb241895f209cc07753c4ef513
jakubowskaD <jakubowska.dj@gmail.com>
2022-06-02 23:27:10 +0200

    BusinessBranchRepository

--- src/main/java/com/event/businessBranch/dao/BusinessBranchRepository.java
+++ src/main/java/com/event/businessBranch/dao/BusinessBranchRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BusinessBranchRepository extends CrudRepository<BusinessBranchModel, Integer> {
+public interface BusinessBranchRepository extends CrudRepository<BusinessBranchModel, String> {
 }
