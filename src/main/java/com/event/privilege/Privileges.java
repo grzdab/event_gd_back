e4bf7d997930aa3c8709263a8642a5c437c83f79
jakubowskaD <jakubowska.dj@gmail.com>
2022-06-13 14:15:30 +0200

    Privileges

--- src/main/java/com/event/privilege/Privileges.java
+++ src/main/java/com/event/privilege/Privileges.java
@@ -1,4 +1,43 @@
 package com.event.privilege;
 
+import java.util.Objects;
+
 public class Privileges {
+
+    String id;
+    String name;
+
+    public Privileges(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Privileges that = (Privileges) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
