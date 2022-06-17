bac01b1ed5898999e046ddc1b4a9fd74db2f29db
jakubowskaD <jakubowska.dj@gmail.com>
2022-06-17 16:31:56 +0200

    ALL - RoleControllerTest

---
+++ src/test/java/com/event/admin/RoleControllerTest.java
@@ -0,0 +1,99 @@
+package com.event.admin;
+
+import com.event.role.Role;
+import com.event.role.RoleController;
+import com.event.role.RoleService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(RoleController.class)
+public class RoleControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    ObjectMapper mapper;
+
+    @MockBean
+    RoleService service;
+
+    Role role1 = new Role(1, "zenek");
+    Role role2 = new Role(2, "olek");
+    Role role3 = new Role(3, "tomek");
+
+    @Test
+    public void getAllRole_success() throws Exception {
+        List<Role> modelList = new ArrayList<>(Arrays.asList(role1, role2, role3));
+        when(service.getAllRoles()).thenReturn(modelList);
+
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders
+                        .get("/admin/role")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String responseContent = mvcResult.getResponse().getContentAsString();
+
+    }
+    @Test
+    public void createPOSTRoleAPI_success() throws Exception {
+        Role model = new Role();
+        model.setId(4);
+        model.setName("mikolaj");
+        service.addRole(model);
+
+        Mockito.when(service.addRole(model)).thenReturn(model);
+
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.post("/admin/role")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(model));
+
+        mvc.perform(mockRequest)
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    public void PUTRoleInfoAPI_success() throws Exception {
+        Role model = new Role();
+        model.setName("michal");
+        service.updateRole(2, model);
+
+        Mockito.when(service.updateRole(model.getId(), model)).thenReturn(model);
+
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.post("/admin/role")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(model));
+
+        mvc.perform(mockRequest)
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void DELETERoleAPI_success() throws Exception{
+        Mockito.when(service.getRole(2)).thenReturn(role2);
+
+        mvc.perform(MockMvcRequestBuilders
+                        .delete("/admin/role/2")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+}
