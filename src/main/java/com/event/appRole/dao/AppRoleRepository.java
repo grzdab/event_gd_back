bdb9d2b1524b7075ad2bdd6bac5236df83e960e6
Grzegorz Dąbrowski <g.dabrowski@onekk.eu>
2022-08-11 12:19:07 +0200

    clean gitignore

--- src/main/java/com/event/appRole/dao/AppRoleRepository.java
+++
@@ -1,8 +0,0 @@
-//package com.event.appRole.dao;
-//
-//import org.springframework.data.repository.CrudRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface AppRoleRepository extends CrudRepository<AppRoleModel, Integer> {
-//}
